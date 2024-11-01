7ee32d63264a09acc567260eda0d21b2765dade7
SalahSoussou <soussousalahiddine.com>
2024-11-01 21:52:34 +0100

    ok

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
 from .models import Book
 
-admin.site.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
 
-"admin.ModelAdmin"
+
+
+admin.site.register(Book, BookAdmin)
+
