54be68c1279a90e6e92840f82787b4ce243b7ff4
SalahSoussou <soussousalahiddine.com>
2024-11-10 20:39:37 +0100

    ok

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -21,3 +21,4 @@ class LibraryDetailView(DetailView):
   template_name = "relationship_app/library_detail.html"
 
 
+"from django.contrib.auth.decorators import permission_required", "relationship_app.can_add_book", "relationship_app.can_change_book", "relationship_app.can_delete_book"
