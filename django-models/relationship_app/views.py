2a8ec227ba5857447ab157bbdacce96b4bf59020
SalahSoussou <soussousalahiddine.com>
2024-11-10 18:36:10 +0100

    ok

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 from .models import Book
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 def book_list(request):
       books = Book.objects.all()
