eb92c359a45ab071e57f90653bfc54d60ee529c5
SalahSoussou <soussousalahiddine.com>
2024-11-10 20:40:21 +0100

    ok

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -3,3 +3,5 @@ from .views import list_books
 "LibraryDetailView", "path"
 
 "views.register", "LogoutView.as_view(template_name=", "LoginView.as_view(template_name="
+
+"add_book/", "edit_book/", "delete_book"
