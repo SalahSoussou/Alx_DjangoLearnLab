5bb7cb5d9f33c7fdb28d1de5e47416390d7d4ba9
SalahSoussou <soussousalahiddine.com>
2024-11-10 17:58:17 +0100

    ok

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -18,3 +18,5 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField(max_length=200)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+"return self.name"
