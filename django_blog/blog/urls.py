c3fa3e7d038746302dc3f9050ed9b81c3abbb677
SalahSoussou <soussousalahiddine.com>
2024-12-06 12:01:54 +0100

    ok

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,13 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("register/", views.register_view, name='register'),
+    # path("login/", views.login_view, name='login'),
+    # path("profile/", views.profile_view, name='profile'),
+    # path("logout/", views.logout_view, name='logout'),
 ]
 
 
-"login/", "register/", "profile/"
